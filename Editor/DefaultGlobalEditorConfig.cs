779c478ecf3139cc0c67028ec1511e850455be75
Serhii Fedorenko <fedorenkosergiy@gmail.com>
2023-06-03 03:22:52 +0300

    add database asset path and guid

--- Editor/DefaultGlobalEditorConfig.cs
+++ Editor/DefaultGlobalEditorConfig.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using static Grabli.Abstraction.Constant;
 
 namespace Exam.Config.Editor
 {
-	[CreateAssetMenu(fileName = nameof(DefaultGlobalEditorConfig) + Grabli.Abstraction.Constant.FileExtension.Asset,
+	[CreateAssetMenu(fileName = nameof(DefaultGlobalEditorConfig) + FileExtension.Asset,
 					 menuName = Constant.MenuPrefix + nameof(DefaultGlobalEditorConfig))]
 	public class DefaultGlobalEditorConfig : ScriptableObject, GlobalEditorConfig
 	{
 		[SerializeField] private DefaultGlobalRuntimeConfig runtimeConfig;
+		[SerializeField] private string databaseAssetGuid;
+		[SerializeField] private string databaseDefaultAssetPath = "Assets/database" + FileExtension.Asset;
 
 		public GlobalRuntimeConfig RuntimeConfig => runtimeConfig;
+
+		public string DatabaseAssetGuid => databaseAssetGuid;
+
+		public string DatabaseDefaultAssetPath => databaseDefaultAssetPath;
 	}
 }
--- Editor/GlobalEditorConfig.cs
+++ Editor/GlobalEditorConfig.cs
@@ -3,5 +3,9 @@ namespace Exam.Config.Editor
 	public interface GlobalEditorConfig
 	{
 		GlobalRuntimeConfig RuntimeConfig { get; }
+
+		string DatabaseAssetGuid { get; }
+
+		string DatabaseDefaultAssetPath { get; }
 	}
 }
